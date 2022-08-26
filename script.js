7ad364bd9e090e61aa088f7b0ab0f22f2d278f3e
Fatima <fatimai.hasanat@gmail.com>
2022-08-26 12:51:51 +0300

    adding sticky-nav-bar-styles and scripts

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 $(document).ready(function(){
-  $(window)
-});
+    $(window).scroll(function(){
+        // sticky navbar on scroll script
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }})});
+        
+        // scroll-up button show/hide script
+     
+    
